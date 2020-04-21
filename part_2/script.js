e81fe137bf5e814f5b74595de662f2bfd63ae68e
Tsyvinskyi Vasiliy <sodiicc@gmail.com>
2020-04-21 19:49:29 +0300

    load another chart

--- part_2/script.js
+++ part_2/script.js
@@ -15,18 +15,22 @@ const fullscreenTitle = 'CPU Utilization (All cores)'
 
 chart.src = `${imgUrl}?${queryParams}`
 
+console.log('window.innerHeight', window.innerHeight)
+
 //Function to resize the chart
-const resizeChart = (element) => {
+const resizeChart = () => {
     if (chart.classList.contains('fullscreen')) {
         chart.classList.remove('fullscreen')
         fullscreen.src = fullscreenUrl
-        fullscreen.style.width = '30px'
         total.innerHTML = title
+        chart.src = `${imgUrl}?${queryParams}`
     } else {
         chart.classList.add('fullscreen')
         fullscreen.src = exitFullscreenUrl
-        fullscreen.style.width = '50px'
         total.innerHTML = fullscreenTitle
+
+        // (viewport height * 95% - header height) it makes to avoid scroll
+        chart.src = `${imgUrl}?width=3600&img_width=${window.innerWidth * 0.9}&img_height=${window.innerHeight * 0.95 -180}`
     }
 }
 
